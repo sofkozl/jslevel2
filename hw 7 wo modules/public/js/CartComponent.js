e92060dc8262b94fdf2db5f00ae1b8ef3e00c181
sofkozl <72669650+sofkozl@users.noreply.github.com>
2021-10-17 20:18:13 +0300

    Add files via upload

---
+++ hw 7 wo modules/public/js/CartComponent.js
@@ -0,0 +1,96 @@
+// const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+Vue.component('cart', {
+    data(){
+      return {
+          cartUrl: '/getBasket.json',
+          cartItems: [],
+          showCart: false
+      }
+    },
+    mounted(){
+        this.$parent.getJson(`/api/cart`)
+            .then(data => {
+                for (let item of data.contents){
+                    item.imgPath = `photo/${item.id_product}.jpg`;
+                    this.$data.cartItems.push(item);
+                }
+            });
+    },
+    methods: {
+        addProduct(item){
+            let find = this.cartItems.find(el => el.id_product === item.id_product);
+            if(find){
+                this.$parent.putJson(`/api/cart/${find.id_product}`, {quantity: 1})
+                    .then(data => {
+                        if(data.result === 1){
+                            find.quantity++
+                        }
+                    })
+            } else {
+                const prod = Object.assign({quantity: 1}, item);
+                this.$parent.postJson(`/api/cart`, prod)
+                    .then(data => {
+                        if(data.result === 1){
+                            this.cartItems.push(prod)
+                        }
+                    })
+            }
+
+            // this.$parent.getJson(`${API}/addToBasket.json`)
+            //     .then(data => {
+            //         if(data.result === 1){
+            //             let find = this.cartItems.find(el => el.id_product === item.id_product);
+            //             if(find){
+            //                 find.quantity++;
+            //             } else {
+            //                 const prod = Object.assign({quantity: 1}, item);
+            //                 this.cartItems.push(prod)
+            //             }
+            //         }
+            //     })
+        },
+        remove(item){
+            this.$parent.getJson(`${API}/addToBasket.json`)
+                .then(data => {
+                    if (data.result === 1) {
+                        if(item.quantity>1){
+                            item.quantity--;
+                        } else {
+                            this.cartItems.splice(this.cartItems.indexOf(item), 1);
+                        }
+                    }
+                })
+        },
+    },
+    template: `<div>
+<button class="btn-cart" type="button" @click="showCart = !showCart">Корзина</button>
+        <div class="cart-block" v-show="showCart">
+            <cart-item v-for="item of cartItems" 
+            :key="item.id_product" 
+            :img="item.imgPath" 
+            :cart-item="item" 
+            @remove="remove">
+            </cart-item>
+        </div>
+        </div>
+    `
+});
+
+Vue.component('cart-item', {
+    props: ['img', 'cartItem'],
+    template: `
+    <div class="cart-item">
+                        <img class="basket-photo-product" :src="img" alt="Some img">
+                        <div class="basket-product-description">
+                            <div class="basket-product-title">{{ cartItem.product_name }}</div>
+                            <div class="basket-product-quantity">Quantity: {{ cartItem.quantity }}</div>
+                            <div class="basket-product-price">$ {{ cartItem.price }} each</div>
+                        </div>
+                    <div class="right-block">
+                        <div class="product-price">{{cartItem.quantity*cartItem.price}}</div>
+                        <button class="del-btn" @click="$emit('remove', cartItem)">&times;</button>
+                    </div>
+                </div>
+    `
+})
